aee9560473e3ee2d0bc77a78454a518683f2a528
mrantonio031 <109715697+mrantonio031@users.noreply.github.com>
2024-05-11 14:47:16 +0200

    Logo

--- subpage/csv-to-html-with-template1.py
+++ subpage/csv-to-html-with-template1.py
@@ -27,7 +27,7 @@ html_template = """
 <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <a class="navbar-brand" href="#">
-            <img src="/img/logo.jpg" alt="Logo">
+            <img src="/img/logo.png" alt="Logo">
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
                 aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
